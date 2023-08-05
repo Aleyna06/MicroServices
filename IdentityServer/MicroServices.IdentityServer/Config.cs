89e719f578eda035f599f23bf7473e14ebc18c43
aleyna çelik <celikaleyna71@gmail.com>
2023-08-05 14:53:52 +0300

    Gün Sonu

--- IdentityServer/MicroServices.IdentityServer/Config.cs
+++ IdentityServer/MicroServices.IdentityServer/Config.cs
@@ -2,57 +2,92 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
+using System;
 using System.Collections.Generic;
 
 namespace MicroServices.IdentityServer
 {
     public static class Config
     {
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+            //yetki ikeyleri
+            new ApiResource("resource_catolog"){ Scopes={ "catalog_fullpermission" } },
+                new ApiResource("resource_photostock"){Scopes={"photostock_fullpermission"}},
+                    new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
+                        new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
+                            new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
+                                new ApiResource("resource_payment"){Scopes={"payment_fullpermission"}},
+                                    new ApiResource("resource_gateway"){Scopes={"gateway_fullpermission"}},
+                                        new ApiResource("resource_cargo"){Scopes={"cargo_fullpermission"}},
+
+                                        new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
+
+        };
+
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
+                          new IdentityResources.Email(),
+                          new IdentityResources.OpenId(),
+                          new IdentityResources.Profile(),
+                          new IdentityResource(){Name="roles",DisplayName="Roles",Description="Kullanıcı Rolleri",UserClaims=new []{"role"}}
                    };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
+                new ApiScope("catalog_fullpermission","Katolog Api İçin erişim "),
+                    new ApiScope("photostock_fullpermission","PhotoStock Api İçin erişim "),
+                        new ApiScope("basket_fullpermission","Sepet Api İçin erişim "),
+                            new ApiScope("discount_fullpermission","Discount Api İçin erişim "),
+                                new ApiScope("order_fullpermission","Sipariş Api İçin erişim "),
+                                    new ApiScope("payment_fullpermission","Payment Api İçin erişim "),
+                                        new ApiScope("gateway_fullpermission","Gateway Api İçin erişim "),
+                                            new ApiScope("cargo_fullpermission","Kargo Api İçin erişim "),
+
+                              new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+
+
             };
 
         public static IEnumerable<Client> Clients =>
             new Client[]
             {
                 // m2m client credentials flow client
-                new Client
+                new Client()
                 {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
+                    ClientName="Casgem1",
+                    ClientId="CoreClient1",
+                    ClientSecrets={new Secret("secret".Sha256())},
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,
+                    AllowedScopes={ "catalog_fullpermission" , "photostock_fullpermission" ,IdentityServerConstants.LocalApi.ScopeName}
 
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
 
-                    AllowedScopes = { "scope1" }
                 },
+                
 
                 // interactive client using code flow + pkce
                 new Client
                 {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                  ClientName=  "Casgem2",
+                  ClientId="CoreClient2",
+                  AllowOfflineAccess=true,
+                  ClientSecrets={new Secret("secret".Sha256())},
+                  AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                  AllowedScopes={ "catalog_fullpermission", "photostock_fullpermission", "basket_fullpermission" , "discount_fullpermission" , "order_fullpermission" , "payment_fullpermission" , "cargo_fullpermission",IdentityServerConstants.StandardScopes.Email,
+                    IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile,
+IdentityServerConstants.StandardScopes.OfflineAccess,IdentityServerConstants.LocalApi.ScopeName,"roles" },
 
-                    AllowedGrantTypes = GrantTypes.Code,
+                AccessTokenLifetime=1*60*60,
+                RefreshTokenExpiration=TokenExpiration.Absolute,
+                AbsoluteRefreshTokenLifetime=(int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
+                RefreshTokenUsage=TokenUsage.ReUse
 
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+                    },
 
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
-                },
             };
     }
 }
